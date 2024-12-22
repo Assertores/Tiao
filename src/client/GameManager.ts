437d4d31f7e40a51337b60303b000c7edbe26809
Andreas Edmeier <assertores@web.de>
2024-12-22 01:48:03 +0100

    add helpers IsMyTurn

--- src/client/GameManager.ts
+++ src/client/GameManager.ts
@@ -2,7 +2,7 @@ import { Board } from '../core/Board'
 import { Cell } from '../core/Cell'
 import { Game } from '../core/Game'
 import { JumpTarget } from '../core/JumpTarget'
-import { PlayerOrder } from '../core/Player'
+import { Player, PlayerOrder } from '../core/Player'
 import { Position } from '../core/Position'
 import { Observable } from './Observable'
 
@@ -10,11 +10,13 @@ export class GameManager {
   public game: Observable<Game | undefined>
   public currentBoard: Observable<Board | undefined>
   public jumpHistory: Observable<{ jump: JumpTarget; board: Board }[]>
+  public me: Observable<Player | undefined>
 
   constructor() {
     this.game = new Observable<Game | undefined>(undefined)
     this.currentBoard = new Observable<Board | undefined>(undefined)
     this.jumpHistory = new Observable<{ jump: JumpTarget; board: Board }[]>([])
+    this.me = new Observable<Player | undefined>(undefined)
   }
 
   public createGame(
@@ -30,15 +32,27 @@ export class GameManager {
   }
 
   public endTurn(): Promise<void> {
+    if (!this.IsMyTurn()) {
+      throw new Error('tryed to make a move eventhow its not his turn')
+    }
+
     throw new Error('not implimented.')
   }
 
   public place(cell: Cell): Promise<void> {
+    if (!this.IsMyTurn()) {
+      throw new Error('tryed to place a stone eventhow its not his turn')
+    }
+
     this.currentBoard.set(cell.place())
     return Promise.resolve()
   }
 
   public jump(target: JumpTarget): Promise<void> {
+    if (!this.IsMyTurn()) {
+      throw new Error('tryed to jump eventhow its not his turn')
+    }
+
     const boardAfterJump = target.jump()
     this.currentBoard.set(boardAfterJump)
 
@@ -51,6 +65,9 @@ export class GameManager {
 
   // index is past end iterator
   public rollback(index: number): Promise<void> {
+    if (!this.IsMyTurn()) {
+      throw new Error('tryed rollback eventhow its not his turn')
+    }
     if (index < 0 || index > this.jumpHistory.value.length) {
       throw new Error('index out of bound')
     }
@@ -58,5 +75,16 @@ export class GameManager {
     this.jumpHistory.set(this.jumpHistory.value.slice(0, index))
     this.currentBoard.set(this.jumpHistory.value[index - 1].board)
     return Promise.resolve()
+  }
+
+  public IsMyTurn(): boolean {
+    if (!this.currentBoard.value) {
+      return false
+    }
+    if (!this.me.value) {
+      throw new Error('game exists but me is not assigned')
+    }
+
+    return this.currentBoard.value.activePlayer === this.me.value.playerOrder
   }
 }
--- src/core/Game.ts
+++ src/core/Game.ts
@@ -9,6 +9,10 @@ export interface Game {
   readonly currentBoard: BoardMemento
 }
 
+function computeActivePlayer(game: Game): Player {
+  return game.players[game.turn % game.players.length]
+}
+
 function isWinConditionReached(game: Game): Player | undefined {
   for (let i = 0; i < game.players.length; i++) {
     if (game.players[i].score >= game.winCondition) {
